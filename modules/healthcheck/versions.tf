a5f7dbde4e6cb47d50612c4182dab5a530d496de
jheyer <jheyer@otldap.net>
2024-10-11 12:53:02 -0700

    update

--- modules/healthcheck/versions.tf
+++ modules/healthcheck/versions.tf
@@ -3,11 +3,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.49.0, < 6.0"
+      version = ">= 4.49.0, < 7.0.0"
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
     }
   }
 }
