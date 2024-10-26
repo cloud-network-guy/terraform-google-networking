e73b3680cd84f0e7e62d63256c02101acff00480
John Heyer <jheyer@otldap.net>
2024-10-26 15:02:46 -0700

    version bump

--- modules/vpc-network/versions.tf
+++ modules/vpc-network/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.16, < 6.0"
+      version = ">= 6.5 < 7.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
