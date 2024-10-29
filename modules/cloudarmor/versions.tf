a352415e68f26a8a2e4bcc5a55160351964d835d
John Heyer <jheyer@otldap.net>
2024-10-29 14:25:12 -0700

    version bump

--- modules/cloudarmor/versions.tf
+++ modules/cloudarmor/versions.tf
@@ -3,15 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.16, < 6.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.4"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.1"
+      version = ">= 5.16, < 7.0"
     }
   }
 }
