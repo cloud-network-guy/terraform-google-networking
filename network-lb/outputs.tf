d5f8a0e8373f92aba17da162972b25a1ebbffe3b
cloud-network-guy <greenlakejohnny@gmail.com>
2024-12-06 20:55:42 -0800

    ouputs

--- network-lb/outputs.tf
+++ network-lb/outputs.tf
@@ -1,1 +1,16 @@
 
+output "health_checks" {
+  value = { for k, v in local.health_checks :
+    k => { for _ in ["name", "id", "self_link"] : _ => lookup(module.healthchecks[k], _, null) }
+  }
+}
+output "backends" {
+  value = { for k, v in local.backends :
+    k => { for _ in ["name", "id", "self_link"] : _ => lookup(module.backends[k], _, null) }
+  }
+}
+output "frontends" {
+  value = { for k, v in local.frontends :
+    k => { for _ in ["name", "id", "self_link"] : _ => lookup(module.frontends[k], _, null) }
+  }
+}
