b49ee86a90eb1c7b3eee3106b5dfee7580c6a58b
John Heyer <jheyer@opentext.com>
2025-06-04 20:44:33 -0700

    updates

--- modules/vpc-network/versions.tf
+++ modules/vpc-network/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.12.0, < 7.0.0"
+      version = ">= 6.12.0, < 6.37.0"
     }
     random = {
       source  = "hashicorp/random"
