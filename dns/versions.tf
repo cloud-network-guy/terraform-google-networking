a7a46494c4bb8b9fb505fffcb66ec3ce7d9ed5f9
John Heyer <jheyer@opentext.com>
2025-06-04 20:11:39 -0700

    test

--- dns/versions.tf
+++ dns/versions.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">= 1.5.7"
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.16.0, < 7.0.0"
+    }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.1"
+      version = ">= 3.1.0"
     }
   }
 }
