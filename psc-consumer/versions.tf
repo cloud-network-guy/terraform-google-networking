44b5dde04c9ff84e6bae4d16049fc0e8ee4e2324
John E. Heyer <jeheyer@me.com>
2026-05-13 13:17:23 -0700

    rewrite of psc consumer module

---
+++ psc-consumer/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.16.0, < 8.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+  }
+  provider_meta "google" {
+    user_agent = [
+      "github.com/cloud-network-guy/terraform-google-networking/psc-consumer"
+    ]
+  }
+}
