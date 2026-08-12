927f79e461295d5c0f91501535e2b681d781ef79
John E. Heyer <jeheyer@me.com>
2026-08-12 13:59:59 -0700

    same

--- modules/vpc-network/outputs.tf
+++ modules/vpc-network/outputs.tf
@@ -1,8 +1,8 @@
 output "project" { value = local.project }
 output "name" { value = local.name }
-output "id" { value = local.network }
 output "numeric_id" { value = local.numeric_id }
 output "network_id" { value = local.network_id }
+output "id" { value = local.network_id }
 output "self_link" { value = local.network_self_link }
 output "subnets" {
   value = [
